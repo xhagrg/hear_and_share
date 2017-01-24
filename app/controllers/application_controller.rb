fe8c7b93e38744b309642ebbe9ce598de20a9f4b
xhagrg <grg.iksha@hotmail.com>
2017-01-24 17:58:16 -0600

    Initialize assets_container.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :reload_assets
+
+  private
+
+  def reload_assets
+    path = Rails.root.join('vendor', 'webpack-assets.json')
+    file = File.read(path)
+    @assets_container = JSON.parse(file)
+  end
 end
+
