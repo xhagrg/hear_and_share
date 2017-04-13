f2f660c8644669be898245f32c65f73c76c7d535
xhagrg <grg.iksha@hotmail.com>
2017-04-13 16:29:26 -0500

    Upgrade angular2 and other packages, details update.

--- vendor/src/vendor.ts
+++ vendor/src/vendor.ts
@@ -1,11 +1,11 @@
 // Angular 2
+import 'reflect-metadata';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
 import '@angular/http';
 import '@angular/router';
-import 'reflect-metadata';
 
 // RxJS
 import 'rxjs';
