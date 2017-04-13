027226ff2b835248a946bb1e85ed7ee3c7f3bf0b
xhagrg <grg.iksha@hotmail.com>
2017-04-13 16:26:43 -0500

    Devise initialization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :reload_assets
+  before_filter :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:nick_name, :first_name, :last_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 
   private
 
---
+++ app/models/user.rb
@@ -0,0 +1,90 @@
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  attr_accessor :login
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, 
+         authentication_keys: [:login]
+
+  ## Database authenticatable
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  field :sign_in_count,      type: Integer, default: 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+  field :first_name,         type: String
+  field :last_name,          type: String
+  field :admin,              type: Boolean,    default: false
+  field :nick_name,          type: String
+  field :friend_id,          type: BSON::ObjectId
+
+  validates :nick_name,
+    presence: true,
+    uniqueness: {
+      case_sensitive: false
+    }
+
+  validate :validate_nickname
+
+  has_many :playlists
+  has_many :sent_invitations, inverse_of: :sender, class_name: 'Invitation'
+  has_many :received_invitations, inverse_of: :receiver, class_name: 'Invitation'
+
+  has_many :friends, inverse_of: :friend, class_name: 'User'
+
+  ## Confirmable
+  # field :confirmation_token,   type: String
+  # field :confirmed_at,         type: Time
+  # field :confirmation_sent_at, type: Time
+  # field :unconfirmed_email,    type: String # Only if using reconfirmable
+
+  ## Lockable
+  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+  # field :locked_at,       type: Time
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.nick_name || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      self.search(query).first
+    else
+      super
+    end
+  end
+
+  def self.search(query)
+    self.any_of(
+      { nick_name:  /^#{Regexp.escape(query)}$/i }, 
+      { email:  /^#{Regexp.escape(query)}$/i }
+    )
+  end
+
+  private
+  def validate_nickname
+    if User.where(email: self.nick_name).exists?
+      errors.add(:nick_name, :invalid)
+    end
+  end
+end
