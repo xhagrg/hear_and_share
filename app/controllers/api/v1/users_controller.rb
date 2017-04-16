3ace680958f9345dbf1a456c363ab58f8f88e42d
xhagrg <grg.iksha@hotmail.com>
2017-04-16 10:27:40 -0500

    View render.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,9 +5,11 @@ class Api::V1::UsersController < Api::V1::ApiBaseController
              else
                User.all
              end
+    render json: @users
   end
 
   def show
     @user = User.where(id: params[:id]).last
+    render json: @user
   end
 end
