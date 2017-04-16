b2cc19a994bc4410034b9814babbffb3e58d1c00
xhagrg <grg.iksha@hotmail.com>
2017-04-16 10:27:31 -0500

    Typo fix.

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Playlists < Api::V1::ApiBaseController
+class Api::V1::PlaylistsController < Api::V1::ApiBaseController
   before_filter :playlist, except: [:index, :create]
   def index
     query = { user_id: params[:user_id] }
@@ -6,14 +6,17 @@ class Api::V1::Playlists < Api::V1::ApiBaseController
       query[:_type] = 'PublicPlaylist'
     end
     @playlists = Playlist.where(query)
+    render json: @playlists
   end
 
   def create
     @playlist = Playlist.new(playlist_params)
     @playlist.save
+    render json: @playlist
   end
 
   def show
+    render json: playlist
   end
 
   def destroy
@@ -28,6 +31,7 @@ class Api::V1::Playlists < Api::V1::ApiBaseController
     elsif(params[:name])
       playlist.update_attributes(name: playlist_params[:name])
     end
+    render json: playlist
   end
 
   private 
