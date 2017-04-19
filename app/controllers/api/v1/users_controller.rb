eca2d2f9711e836146f5f2b4f68320a40b4677b5
xhagrg <grg.iksha@hotmail.com>
2017-04-19 09:44:48 -0500

    List all the users except the current_user.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::UsersController < Api::V1::ApiBaseController
   def index
+    users = User.where(:id.ne => params[:current_user_id])
     @users = if(params[:query]) 
-               User.search(params[:query])
+               users.search(params[:query])
              else
-               User.all
+               users.all
              end
-    render json: @users
+    render json: @users, current_user_id: params[:current_user_id]
   end
 
   def show
     @user = User.where(id: params[:id]).last
-    render json: @user
+    render json: @user, current_user_id: params[:current_user_id]
   end
 end
