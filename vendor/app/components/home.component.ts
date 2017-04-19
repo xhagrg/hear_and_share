3e2239b023a184fca87261e4c8a88cf0b5004418
xhagrg <grg.iksha@hotmail.com>
2017-04-19 09:49:46 -0500

    Home component to handle requests other than player control.

--- vendor/app/components/home.component.ts
+++ vendor/app/components/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, OnInit, Input } from '@angular/core';
+import { InvitationService } from '../services/invitation.service';
 
 @Component({
   selector: 'home-selector',
@@ -8,6 +9,32 @@ import { Component, ViewChild, OnInit, Input } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void { }
- }
+  public selectedComponent: string;
+  public componentList;
+  public invitationCount;
+  public currentUserId;
+
+  constructor(private invtServ: InvitationService) {
+    this.selectedComponent = 'Playlists';  
+    this.componentList = ['Invitations', 'Users', 'Friends', 'Playlists'];
+  }
+
+  ngOnInit(): void {
+    this.currentUserId = document.getElementById('current_user_id').dataset.currentUserId;
+    this.retrieveInvitations();
+    setInterval(() => {
+      this.retrieveInvitations();
+    }, 5000);
+  }
+
+  public retrieveInvitations() {
+    this.invtServ.invitations(this.currentUserId).then((response) => 
+      this.invitationCount = response.length
+    );
+  }
+
+  public updateComponent(component) {
+    this.selectedComponent = component;
+  }
+
+}
