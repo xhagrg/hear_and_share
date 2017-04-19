697e454544d1216d196fcb87308415bc098eb978
xhagrg <grg.iksha@hotmail.com>
2017-04-19 09:45:38 -0500

    Send sender_name for invitation.

--- app/serializers/invitation_serializer.rb
+++ app/serializers/invitation_serializer.rb
@@ -1,7 +1,11 @@
 class InvitationSerializer < ActiveModel::Serializer
-  attributes :id, :message
+  attributes :id, :message, :sender_name, :sender_id, :receiver_id
 
   def id
     object._id.to_s
+  end
+
+  def sender_name
+    object.sender.name
   end
 end
