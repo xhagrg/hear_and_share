fb074a42fcc5ffd8b975c7c4b6ebdfb185368fd0
xhagrg <grg.iksha@hotmail.com>
2017-04-19 09:48:30 -0500

    Inport all new services.

--- vendor/app/app.module.ts
+++ vendor/app/app.module.ts
@@ -11,17 +11,27 @@ import { YoutubePlayerModule } from 'ng2-youtube-player';
 
 import { AppComponent } from './components/app.component';
 import { HomeComponent } from './components/home.component';
+import { InvitationComponent } from '../app/components/invitation.component';
+import { PlaylistDetailComponent } from '../app/components/playlist-detail.component';
+import { PlaylistListComponent } from '../app/components/playlist-list.component';
+import { UserListComponent } from '../app/components/user-list.component';
+import { UserDetailComponent } from '../app/components/user-detail.component';
+import { FriendListComponent } from '../app/components/friend-list.component';
 
 // import { NavigatorComponent } from './components/navigator.component';
 // import { MarkerComponent } from './components/marker.component';
 
 // import { EventService } from './services/event.service';
-// import { MapService } from './services/map.service';
+import { PlaylistService } from './services/playlist.service';
+import { UserService } from './services/user.service';
+import { InvitationService } from './services/invitation.service';
+import { SongEventService } from './services/song-event.service';
+
 // import { GeocodingService } from './services/geocoding.service';
 
 import { LoadersCssModule } from 'angular2-loaders-css';
 import { TooltipModule } from "ng2-tooltip";
-
+import { ModalModule } from "ngx-modal";
 
 import { routing } from '../config/app.routing';
 
@@ -33,18 +43,24 @@ import { routing } from '../config/app.routing';
     YoutubePlayerModule,
     LoadersCssModule,
     TooltipModule,
+    ModalModule,
     routing
   ],
   declarations: [
     AppComponent,
     HomeComponent,
-    // NavigatorComponent,
-    // MarkerComponent
+    InvitationComponent,
+    PlaylistDetailComponent,
+    PlaylistListComponent,
+    UserListComponent,
+    FriendListComponent,
+    UserDetailComponent
   ],
   providers: [
-    // MapService,
-    // GeocodingService,
-    // EventService,
+    InvitationService,
+    PlaylistService,
+    UserService,
+    SongEventService,
     { provide: APP_BASE_HREF, useValue: '' }
   ],
   bootstrap: [AppComponent]
