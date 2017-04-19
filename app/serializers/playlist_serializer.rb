97385ab7a2f95c3bdbc46ea5d74b264fb09c06a6
xhagrg <grg.iksha@hotmail.com>
2017-04-19 09:45:57 -0500

    Send all required information for playlists.

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,8 +1,24 @@
 class PlaylistSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :user_id, :user_name, :type, :error
   has_many :songs
 
   def id
     object._id.to_s
+  end
+
+  def user_id
+    object.user_id.to_s
+  end
+
+  def user_name
+    object.user.name
+  end
+
+  def type
+    object._type
+  end
+
+  def error
+    object.errors.full_messages
   end
 end
