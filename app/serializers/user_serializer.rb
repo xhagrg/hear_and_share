6c867b2b92b09e09573b773ff774d22875df7137
xhagrg <grg.iksha@hotmail.com>
2017-04-19 09:46:08 -0500

    Send required info for users.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,32 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :name, :nick_name, :email
+  attributes :id, :name, :nick_name, :email, :is_friend, 
+             :invitation_present
+  has_many :friends
 
   def id
     object._id.to_s
+  end
+
+  def friends
+    if @instance_options[:current_user_id] == id
+      object.friends
+    else
+      []
+    end
+  end
+
+  def is_friend
+    current_user.friend_ids.map(&:to_s).include?(id)
+  end
+
+  def invitation_present
+    Invitation.or([
+      { sender_id: id, receiver_id: current_user.id }, 
+      { sender_id: current_user.id, receiver_id: id }
+    ]).present?
+  end
+
+  def sent_invitation
+    current_user.sent_invitations.map { |i| i.receiver_id.to_s }.include?(id)
   end
 end
