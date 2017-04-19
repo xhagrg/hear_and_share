87ae4f89a04ac51a1793f279d71e2731fe1d7855
xhagrg <grg.iksha@hotmail.com>
2017-04-19 09:47:46 -0500

    Routes update.

--- vendor/config/app.routing.ts
+++ vendor/config/app.routing.ts
@@ -2,6 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../app/components/home.component';
+import { InvitationComponent } from '../app/components/invitation.component';
+import { PlaylistDetailComponent } from '../app/components/playlist-detail.component';
+import { PlaylistListComponent } from '../app/components/playlist-list.component';
+import { UserListComponent } from '../app/components/user-list.component';
+import { UserDetailComponent } from '../app/components/user-detail.component';
+
 // import { CollectionDetailComponent } from '../app/components/collection-detail.component';
 // import { CollectionsComponent } from '../app/components/collections.component';
 // import { ChartComponent } from '../app/components/chart.component';
@@ -12,6 +18,26 @@ const appRoutes: Routes = [
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
+  },
+  {
+    path: 'invitations',
+    component: InvitationComponent
+  },
+  {
+    path: 'users',
+    component: UserListComponent
+  },
+  {
+    path: 'playlists',
+    component: PlaylistListComponent
+  },
+  { 
+    path: 'playlists/:id', 
+    component: PlaylistDetailComponent 
+  },
+  { 
+    path: 'users/:id', 
+    component: UserDetailComponent 
   },
   {
     path: 'home',
